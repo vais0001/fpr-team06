2359e03c482a2c42efdc6f4247c3aa80a626cdfb
Kristers Davis Gruzins <112697910+animepuika@users.noreply.github.com>
2023-05-17 21:45:01 +0200

    ADDED NAVBAR
    added nav bar

--- resources/views/model.blade.php
+++ resources/views/model.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head lang="en">
     <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <title>My first three.js app</title>
     <style>
         body { margin: 0; padding: 0; overflow: hidden}
@@ -22,8 +24,44 @@
             border: 1px solid #ccc;
             cursor: pointer;
         }
+        #jrczimg {
+            height: 120px;
+            width: 200px;
+        }
+        .logo {
+            height: 50px;
+            width: 50px;
+        }
     </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+<body class="antialiased d-flex flex-column">
+<nav class="navbar navbar-expand-lg flex-wrap navbar-light bg-light">
+    <img id='jrczimg' src="{{ asset('images/jrcz.png') }}" alt="JRCZ-logo" class="">
+    <ul class="navbar-nav d-flex flex-grow-1 justify-content-left">
+        <li class="nav-item active">
+            Welcome, [Person]!
+        </li>
+    </ul>
+    <ul class="navbar-nav d-flex flex-grow-1 justify-content-left">
+        <li class="nav-item active ">
+            Room Number
+        </li>
+    </ul>
+    <ul class="navbar-nav d-flex flex-grow-3 justify-content-right">
+        <li class="nav-item active ">
+            <a class="nav-link text-primary" href="#"> Upload </a>
+        </li>
+    </ul>
+    <ul class="navbar-nav d-flex flex-grow-3 justify-content-right">
+        <li class="nav-item d-flex gap-1" style="margin-left: auto">
+            <a class="nav-link" href="#"><img  src="{{ asset('images/internet.png') }}" alt="internet-logo" class="logo"></a>
+            <a class="nav-link" href="#"><img  src="{{ asset('images/bell.png') }}" alt="bell-logo" class="logo"></a>
+            <a class="nav-link" href="#"><img  src="{{ asset('images/user.png') }}" alt="user-logo" class="logo"></a>
+        </li>
+    </ul>
+</nav>
+
     <div class="button-container">
         <button class="button" value="ground">Ground Floor</button>
         <button class="button" value="1">Floor 1</button>
@@ -31,6 +69,5 @@
         <button class="button" value="3">Floor 3</button>
     </div>
 </head>
-<body>
 </body>
 </html>
