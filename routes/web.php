08ba02a4c4bad252ff0cc0dcaa3e85eb4a437204
vais0001 <vais0001@hz.nl>
2023-05-25 11:05:04 +0200

    fixed welcome not found

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('model');
+})->middleware(['auth', 'verified'])->name('model');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
