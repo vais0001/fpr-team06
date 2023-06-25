b52cbfac7dcbf78b4722bfa40b9b090f80cd6b6d
Baba-ott <inig0001@hz.nl>
2023-06-25 22:01:11 +0200

    admin role to import page fix

--- config/database.php
+++ config/database.php
@@ -65,20 +65,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
