37670f81b55299bf1861a7ac55cc8fa03584cb29
vais0001 <vais0001@hz.nl>
2023-06-26 13:38:29 +0200

    added admin user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'jrcz@hz.nl',
+            'password' => Hash::make('Jrcz2023'), // Change 'password' to your desired password
+            'is_admin' => 1,
+        ]);
     }
 }
